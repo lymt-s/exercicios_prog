fbff0edf765f63d16e7cbb341d1891988255b220
lymt-s <up202306308@up.pt>
2025-02-19 15:00:13 +0000

    Resolução do exercício 1 da Ficha 01 atualizado.

---
+++ src/aula1/exercicio1/Main.java
@@ -0,0 +1,22 @@
+package aula1.exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cilindro c1 = new Cilindro();
+        double volume;
+
+        c1.setRaio(5);
+        c1.setAltura(10);
+        volume = c1.calcularVolume();
+        System.out.println("O volume do cilindro é: " + volume);
+
+
+        Cilindro c2 = new Cilindro();
+
+        c2.setRaio(6);
+        c2.setAltura(12);
+        volume = c2.calcularVolume();
+        System.out.println("O volume do novo cilindro é: " + volume);
+
+    }
+}
