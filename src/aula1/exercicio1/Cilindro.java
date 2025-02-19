957e75ab7d1d57fb99bea9f1e7675280cb186e0b
lymt-s <up202306308@up.pt>
2025-02-19 15:06:00 +0000

    Adição de comentários no exercício 1 da ficha 01, de forma a facilitar uma primeira interação com objetos e métodos de instância.

--- src/aula1/exercicio1/Cilindro.java
+++ src/aula1/exercicio1/Cilindro.java
@@ -1,17 +1,17 @@
 package aula1.exercicio1;
 
-public class Cilindro {
+public class Cilindro { //classes sempre c/letra maiuscula, permitem criar objeto
     private double raio;
     private double altura;
 
-    public double getRaio() {
+    public double getRaio() { //pode ser invocado fora do objeto (public)
         return raio;
     }
 
-    public void setRaio(double raio) {
-        this.raio = raio;
+    public void setRaio(double raio) { //metodo de modificacao (set): altera o valor do atributo
+        this.raio = raio; // "this" referência para o proprio objeto - o raio DESTE objeto
     }
-
+    //recebem um valor por parametro que coincide com o valor do atributo
     public double getAltura() {
         return altura;
     }
@@ -19,6 +19,9 @@ public class Cilindro {
     public void setAltura(double altura) {
         this.altura = altura;
     }
+
+    //todos os acima sao metodos de instância: estao associados a um objeto
+    //o valor dos atributos distingue os objetos da classe
 
     public double calcularVolume() {
         double volume = Math.PI * Math.pow(raio, 2) * altura;
